e6a603de7775a627897dbb292079d8aefc207ec1
DexterWymiatacz <dexterwymiatacz@gmail.com>
2021-05-09 23:34:04 +0200

    #21 - Building & Deploying to Netlify

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,19 +1,44 @@
+import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from 'react'
 import { Layout } from '../components/Layout'
 import * as styles from '../styles/project-details.module.css'
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+    const { html } = data.markdownRemark
+    const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+
     return (
         <Layout>
             <div className={styles.details}>
-                <h2>title</h2>
-                <h3>stack</h3>
+                <h2>{ title }</h2>
+                <h3>{ stack }</h3>
                 <div className={styles.featured}>
-                    {/* <GatsbyImage image={getImage}/> */}
+                    <GatsbyImage image={getImage(featuredImg.childImageSharp.gatsbyImageData)}/>
                 </div>
-                {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+                <div className={styles.html} dangerouslySetInnerHTML={{ __html: html }} />
             </div>
         </Layout>
     )
 }
+
+export const query = graphql`
+query ProjectsDetails($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData(
+                layout: FULL_WIDTH
+                placeholder: BLURRED
+                formats: [AUTO, AVIF]
+            )
+          }
+        }
+      }
+    }
+  }
+`
